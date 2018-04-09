ca91de53fc032741b3889a9b96d02a28bd372e5b
Michel B <michelb@JB-MBA.local>
2018-04-08 20:29:27 -0400

    corrected axios endpoint for family members to display again

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
 	}
 
 	componentDidMount(){
-		axios.get("/api")
+		axios.get("/api/family")
 			.then((response)=>{
 				console.log(response.data);
 				this.setState({
