bcfcf287638003227565fbee2109d2ab8df15fb7
Michel B <michelb@JB-MBA.local>
2018-04-11 23:42:13 -0400

    another attempt at prescription home

--- client/src/components/FamilyMemberCard/FamilyMemberCard.js
+++ client/src/components/FamilyMemberCard/FamilyMemberCard.js
@@ -1,39 +1,22 @@
 import React from 'react';
-import API from '../../utils/API.js'
-import ToolBar from '../ToolBar/ToolBar.js';
 import { Card, CardTitle } from 'material-ui/Card';
 
-class FamilyMemberCard extends React.Component {
-    state = {
-        familymembers: {}
-    }
-
-    componentDidMount() {
-        API.findOneMember(this.props.match.params.id)
-            .then(res => this.setState({ familymembers: res.data }))
-            .catch(err => console.log(err));
-    }
-
-    render() {
-        return (
-            <div>
-                <ToolBar />
-                <div align="center">
-                    <Card>
-                        <CardTitle title={this.state.familymembers.firstName + "'s Profile & Prescriptions"} />
-                        <p><strong>First Name:</strong> {this.state.familymembers.firstName}
-                            <br />
-                            <strong>Last Name:</strong> {this.state.familymembers.lastName}
-                            <br />
-                            <strong>Date of Birth:</strong> {this.state.familymembers.dob}
-                            <br />
-                            <strong>Address:</strong> {this.state.familymembers.address}
-                        </p>
-                    </Card>
-                </div>
-            </div>
-        )
-    }
+const FamilyMemberCard = (props) => {
+    return (
+        <div align="center">
+            <Card>
+                <CardTitle title={props.familyMember.firstName + "'s Profile & Prescriptions"} />
+                <p><strong>First Name:</strong> {props.familyMember.firstName}
+                    <br />
+                    <strong>Last Name:</strong> {props.familyMember.lastName}
+                    <br />
+                    <strong>Date of Birth:</strong> {props.familyMember.dob}
+                    <br />
+                    <strong>Address:</strong> {props.familyMember.address}
+                </p>
+            </Card>
+        </div>
+    )
 }
 
 export default FamilyMemberCard;
--- client/src/components/PrescriptionHome/PrescriptionHome.js
+++ client/src/components/PrescriptionHome/PrescriptionHome.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import API from '../../utils/API.js'
+import ToolBar from '../ToolBar/ToolBar.js';
 import FamilyMemberCard from '../FamilyMemberCard/FamilyMemberCard.js'
 
-const PrescriptionHome = () => {
-	return (
-		<div>
-			<FamilyMemberCard />
-		</div>
-	)
+class PrescriptionHome extends React.Component {
+	state = {
+		familyMember: {}
+	};
+
+	componentDidMount() {
+		API.findOneMember(this.props.match.params.id)
+			.then(res => this.setState({ familyMember: res.data }))
+			.catch(err => console.log(err));
+	}
+
+	render() {
+		return (
+			<div>
+				<ToolBar />
+				<FamilyMemberCard familyMember={this.state.familyMember} />
+			</div>
+		);
+	}
 }
 
 export default PrescriptionHome;
